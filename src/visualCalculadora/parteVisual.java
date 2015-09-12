b4558f9b0b630ccff3bcce31bd3c35a4c3b7bf9d
Usuario <Usuario@Intel>
2015-09-12 16:21:09 -0500

    Cambios en la ventana (Adicion de menú y un item) y en el nombre del proyecto

--- src/visualCalculadora/parteVisual.java
+++ src/visualCalculadora/parteVisual.java
@@ -5,16 +5,44 @@
  */
 package visualCalculadora;
 
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
 /**
  *
  * @author Usuario
  */
-public class parteVisual extends JFrame{
-    
+public class parteVisual extends JFrame implements ActionListener {
+
     private JMenuBar barraMenu;
-   private JTextField txtEspacio; 
+    private JTextField txtEspacio;
+    private JMenuBar mb;
+    private JMenu menu1;
+    private JMenuItem mi1, mi2, mi3;
+
+    public parteVisual() {
+        setLayout(null);
+        mb = new JMenuBar();
+        setJMenuBar(mb);
+        menu1 = new JMenu("Opciones");
+        mb.add(menu1);
+        mi1 = new JMenuItem("Creditos");
+        mi1.addActionListener(this);
+        menu1.add(mi1);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        Container f = this.getContentPane();
+        if (ae.getSource() == mi1) {
+            JOptionPane.showMessageDialog(null, "Calculadora elaborada por: \n Juan Pablo Montoya Restrepo \n Sebastian Moncada \n Miguel Mejia");
+        }
+    }
 }
