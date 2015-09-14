e030d06ffd3b1357804a932bcff4e385e2117a51
Usuario <Usuario@Intel>
2015-09-14 17:42:56 -0500

    Cambios en la parte visual y en la logica de las operaciones

--- src/visualCalculadora/parteVisual.java
+++ src/visualCalculadora/parteVisual.java
@@ -6,18 +6,22 @@
 package visualCalculadora;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.Container;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.Image;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
 import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
+import javax.swing.InputVerifier;
 import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
@@ -42,11 +46,16 @@ public class parteVisual extends JFrame implements ActionListener {
     private JMenuItem mi1, mi2;
     private JButton btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn0;
     private JButton btnSuma, btnResta, btnProducto, btnDivision, btnBackSpace, btnIgual;
+    private JButton btnBorrar, btnParIzquierdo, btnParDerecho, btnPunto;
     private JTextArea txtConsola;
     private ArrayStack<String> pila;
     private ArrayList<String> expresion = new ArrayList<>();
     private ArrayList<String> expresion2 = new ArrayList<>();
     private String consola;
+    private String par;
+    private String cadenaPunto;
+    private int ParDer, ParIzq;
+    private boolean comienza, termina;
 
     public parteVisual() {
 
@@ -57,19 +66,15 @@ public class parteVisual extends JFrame implements ActionListener {
         pane.add(panelBotones(), BorderLayout.CENTER);
         pila = new ArrayStack<>();
         pila.push("");
-        mb = new JMenuBar();
-        setJMenuBar(mb);
-        menu1 = new JMenu("Opciones");
-        mb.add(menu1);
-        mi1 = new JMenuItem("Créditos");
-        mi1.addActionListener(this);
-        menu1.add(mi1);
-        menu1 = new JMenu("Opciones");
-        mb.add(menu1);
-        mi2 = new JMenuItem("Borrar");
-        mi2.addActionListener(this);
-        menu1.add(mi2);
 
+         mb = new JMenuBar();
+         setJMenuBar(mb);
+         menu1 = new JMenu("Opciones");
+         mb.add(menu1);
+         mi1 = new JMenuItem("Créditos");
+         mi1.addActionListener(this);
+         menu1.add(mi1);
+         
         try {
             Image img = ImageIO.read(new File("calculator.png"));
             this.setIconImage(img);
@@ -82,112 +87,191 @@ public class parteVisual extends JFrame implements ActionListener {
         JPanel inner = new JPanel();
         inner.setLayout(new GridLayout(1, 1, 5, 5));
         txtConsola = new JTextArea(3, 50);
-        Font font = new Font("Verdana", Font.BOLD, 18);
+        Font font = new Font("Arial", Font.BOLD, 18);
         txtConsola.setFont(font);
-        txtConsola.setEditable(false);
+        txtConsola.setEditable(true);
         txtConsola.setText("");
         inner.add(this.txtConsola);
         return inner;
 
     }
 
-    @Override
-    public void actionPerformed(ActionEvent ae) {
-        Container f = this.getContentPane();
-        if (ae.getSource() == mi1) {
-            JOptionPane.showMessageDialog(null, "Calculadora elaborada por: \n \n Juan Pablo Montoya Restrepo \n Sebastian Moncada Duque \n Miguel Mejia");
-        }
-        if (ae.getSource() == mi2) {
-            JOptionPane.showMessageDialog(null, "Se han borrado los datos");
-            while (!pila.isEmpty()) {  
-                pila.push("");
-                pila.pop();
-                txtConsola.setText("");
-            }
-        }
-        
-        JButton boton = (JButton) ae.getSource();
-        String texto = boton.getText();
-        if (pila.peek().equals("")) {
-            txtConsola.setText("");
-        }
-        if (!((pila.peek().equals("+") || pila.peek().equals("-") || pila.peek().equals("*") || pila.peek().equals("/")) && (texto.equals("+") || texto.equals("-") || texto.equals("*") || texto.equals("/")))) {
-            if (!(texto.equals("bks") || texto.equals("="))) {
-                pila.push(texto);
-                txtConsola.setText(txtConsola.getText() + texto);
-            }
-            if (texto.equals("bks") && !pila.peek().equals("")) {
-                pila.pop();
-                consola = txtConsola.getText();
-                consola = consola.substring(0, consola.length() - 1);
-                txtConsola.setText(consola);
-            }
-            if (texto.equals("=") && !pila.peek().equals("")) {
-                resultado();
-            }
-        }
-    }
-
     protected JComponent panelBotones() {
         JPanel inner = new JPanel();
-        inner.setLayout(new GridLayout(4, 4, 5, 5));
+        inner.setLayout(new GridLayout(4, 5, 5, 5));
         inner.setSize(5, 5);
         inner.setBorder(BorderFactory.createEmptyBorder(3, 3, 3, 3));
         btn1 = new JButton("1");
         inner.add(btn1);
         btn1.addActionListener(this);
+        btn1.setBackground(Color.darkGray);
+        btn1.setForeground(Color.WHITE);
         btn2 = new JButton("2");
+        btn2.setBackground(Color.darkGray);
+        btn2.setForeground(Color.WHITE);
         inner.add(btn2);
         btn2.addActionListener(this);
         btn3 = new JButton("3");
+        btn3.setBackground(Color.darkGray);
+        btn3.setForeground(Color.WHITE);
         inner.add(btn3);
         btn3.addActionListener(this);
         btnSuma = new JButton("+");
+        btnSuma.setBackground(Color.darkGray);
+        btnSuma.setForeground(Color.WHITE);
         inner.add(btnSuma);
         btnSuma.addActionListener(this);
+        btnParIzquierdo = new JButton("(");
+        btnParIzquierdo.setBackground(Color.darkGray);
+        btnParIzquierdo.setForeground(Color.WHITE);
+        inner.add(btnParIzquierdo);
         btn4 = new JButton("4");
+        btn4.setBackground(Color.darkGray);
+        btn4.setForeground(Color.WHITE);
         inner.add(btn4);
         btn4.addActionListener(this);
         btn5 = new JButton("5");
+        btn5.setBackground(Color.darkGray);
+        btn5.setForeground(Color.WHITE);
         inner.add(btn5);
         btn5.addActionListener(this);
         btn6 = new JButton("6");
+        btn6.setBackground(Color.darkGray);
+        btn6.setForeground(Color.WHITE);
         inner.add(btn6);
         btn6.addActionListener(this);
         btnResta = new JButton("-");
+        btnResta.setBackground(Color.darkGray);
+        btnResta.setForeground(Color.WHITE);
         inner.add(btnResta);
         btnResta.addActionListener(this);
+        btnParDerecho = new JButton(")");
+        btnParDerecho.setBackground(Color.darkGray);
+        btnParDerecho.setForeground(Color.WHITE);
+        inner.add(btnParDerecho);
         btn7 = new JButton("7");
+        btn7.setBackground(Color.darkGray);
+        btn7.setForeground(Color.WHITE);
         inner.add(btn7);
         btn7.addActionListener(this);
         btn8 = new JButton("8");
+        btn8.setBackground(Color.darkGray);
+        btn8.setForeground(Color.WHITE);
         inner.add(btn8);
         btn8.addActionListener(this);
         btn9 = new JButton("9");
+        btn9.setBackground(Color.darkGray);
+        btn9.setForeground(Color.WHITE);
         inner.add(btn9);
         btn9.addActionListener(this);
         btnProducto = new JButton("*");
+        btnProducto.setBackground(Color.darkGray);
+        btnProducto.setForeground(Color.WHITE);
         inner.add(btnProducto);
         btnProducto.addActionListener(this);
-        btnBackSpace = new JButton("bks");
+        btnBackSpace = new JButton("<-");
+        btnBackSpace.setBackground(Color.darkGray);
+        btnBackSpace.setForeground(Color.WHITE);
         inner.add(btnBackSpace);
+        btnPunto = new JButton(".");
+        btnPunto.setBackground(Color.darkGray);
+        btnPunto.setForeground(Color.WHITE);
+        inner.add(btnPunto);
+        btnPunto.addActionListener(this);
         btnBackSpace.addActionListener(this);
         btn0 = new JButton("0");
+        btn0.setBackground(Color.darkGray);
+        btn0.setForeground(Color.WHITE);
         inner.add(btn0);
         btn0.addActionListener(this);
         btnIgual = new JButton("=");
+        btnIgual.setBackground(Color.darkGray);
+        btnIgual.setForeground(Color.WHITE);
         inner.add(btnIgual);
         btnIgual.addActionListener(this);
         btnDivision = new JButton("/");
+        btnDivision.setBackground(Color.darkGray);
+        btnDivision.setForeground(Color.WHITE);
         inner.add(btnDivision);
         btnDivision.addActionListener(this);
+        btnBorrar = new JButton("C");
+        btnBorrar.setBackground(Color.darkGray);
+        btnBorrar.setForeground(Color.WHITE);
+        inner.add(btnBorrar);
+        btnBorrar.addActionListener(this);
+
         return inner;
     }
 
+    @Override
+    public void actionPerformed(ActionEvent evento) {
+
+        Container f = this.getContentPane();
+        if (evento.getSource() == mi1) {
+            JOptionPane.showMessageDialog(null, "Calculadora elaborada por: \n \n Juan Pablo Montoya Restrepo \n Sebastian Moncada Duque \n Miguel Mejia");
+        }
+
+        JButton boton = (JButton) evento.getSource();
+        String texto = boton.getText();
+        if (pila.peek().equals("")) {
+            txtConsola.setText("");
+        }
+        if (!((pila.peek().equals("+") || pila.peek().equals("-") || pila.peek().equals("*") || pila.peek().equals("/")) && (texto.equals("+") || texto.equals("-") || texto.equals("*") || texto.equals("/")))) {
+            if (!(texto.equals("<-") || texto.equals("=") || texto.equals(".") || texto.equals("C"))) {
+                cadenaPunto = cadenaPunto + texto;
+                pila.push(texto);
+                txtConsola.setText(txtConsola.getText() + texto);
+                if (texto.equals("*") || texto.equals("/") || texto.equals("+") || texto.equals("-")) {
+                    cadenaPunto = "";
+                }
+                if (texto.equals("(")) {
+                    ParIzq++;
+                }
+                if (texto.equals(")")) {
+                    ParDer++;
+                }
+
+            }
+            if (texto.equals("<-") && !pila.peek().equals("")) {
+                pila.pop();
+                consola = txtConsola.getText();
+                consola = consola.substring(0, consola.length() - 1);
+                cadenaPunto = cadenaPunto.substring(0, cadenaPunto.length() - 1);
+                txtConsola.setText(consola);
+            }
+            if (texto.equals("C") && !pila.peek().equals("")) {
+                while (!pila.isEmpty()) {
+                    pila.pop();
+                }
+                pila.push("");
+                txtConsola.setText("");
+                cadenaPunto = "";
+            }
+            if (texto.equals(".") && !(pila.peek().equals("+") || pila.peek().equals("-") || pila.peek().equals("*") || pila.peek().equals("/")) && !pila.peek().equals("")) {
+                if (!cadenaPunto.contains(".")) {
+                    pila.push(texto);
+                    txtConsola.setText(txtConsola.getText() + texto);
+                    cadenaPunto = cadenaPunto + texto;
+                } else {
+                    JOptionPane.showMessageDialog(parteVisual.this, "Entrada invalida");
+                }
+            }
+            if (texto.equals("=") && !pila.peek().equals("")) {
+                if (ParDer == ParIzq) {
+                    expresion.clear();
+                    resultado();
+                } else {
+                    JOptionPane.showMessageDialog(parteVisual.this, "Numero erroneo de parentesis");
+                }
+            }
+        }
+
+    }
+
     public void resultado() {
-       String n = "";
+        String n = "";
         while (!pila.isEmpty()) {
-            if (!(pila.peek().equals("+") || pila.peek().equals("-") || pila.peek().equals("*") || pila.peek().equals("/"))) {
+            if (!(pila.peek().equals("+") || pila.peek().equals("-") || pila.peek().equals("*") || pila.peek().equals("/") || pila.peek().equals("(") || pila.peek().equals(")"))) {
                 n = pila.pop() + n;
                 if (pila.isEmpty()) {
                     expresion.add(n);
@@ -200,11 +284,12 @@ public class parteVisual extends JFrame implements ActionListener {
             }
         }
         Collections.reverse(expresion);
+        expresion.removeAll(Collections.singleton(""));
         System.out.println(expresion);
         while (!(expresion.size() == 1)) {
             while (expresion.contains("*")) {
                 for (int i = 0; i < expresion.size(); i++) {
-                    if ("*".equals(expresion.get(i))) {
+                    if (expresion.get(i) == "*") {
                         double num1 = Double.parseDouble(expresion.get(i - 1));
                         double num2 = Double.parseDouble(expresion.get(i + 1));
                         String res = num1 * num2 + "";
@@ -217,9 +302,12 @@ public class parteVisual extends JFrame implements ActionListener {
             }
             while (expresion.contains("/")) {
                 for (int i = 0; i < expresion.size(); i++) {
-                    if ("/".equals(expresion.get(i))) {
+                    if (expresion.get(i) == "/") {
                         double num1 = Double.parseDouble(expresion.get(i - 1));
                         double num2 = Double.parseDouble(expresion.get(i + 1));
+                        if (num2 == 0) {
+                            JOptionPane.showMessageDialog(parteVisual.this, "La division por cero no esta definida");
+                        }
                         String res = num1 / num2 + "";
                         expresion.set(i, res);
                         expresion.set(i - 1, "");
@@ -230,7 +318,7 @@ public class parteVisual extends JFrame implements ActionListener {
             }
             while (expresion.contains("+")) {
                 for (int i = 0; i < expresion.size(); i++) {
-                    if ("+".equals(expresion.get(i))) {
+                    if (expresion.get(i) == "+") {
                         double num1 = Double.parseDouble(expresion.get(i - 1));
                         double num2 = Double.parseDouble(expresion.get(i + 1));
                         String res = num1 + num2 + "";
@@ -243,7 +331,7 @@ public class parteVisual extends JFrame implements ActionListener {
             }
             while (expresion.contains("-")) {
                 for (int i = 0; i < expresion.size(); i++) {
-                    if ("-".equals(expresion.get(i))) {
+                    if (expresion.get(i) == "-") {
                         double num1 = Double.parseDouble(expresion.get(i - 1));
                         double num2 = Double.parseDouble(expresion.get(i + 1));
                         String res = num1 - num2 + "";
@@ -254,8 +342,9 @@ public class parteVisual extends JFrame implements ActionListener {
                     }
                 }
             }
+
         }
-        txtConsola.setText(expresion.get(0));
+        txtConsola.setText(txtConsola.getText() + "\n\n" + " = " + expresion.get(0));
         pila.push("");
     }
 }
